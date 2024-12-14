5ae2d24746b919f5b31fd0614e1120021305e173
이창근 <dlckdrms0517@gmail.com>
2024-12-14 14:08:28 +0900

    feat: 경고/면담/제적 대상자 도메인 작성

---
+++ src/main/java/attendance/domain/AttendanceInterview.java
@@ -0,0 +1,26 @@
+package attendance.domain;
+
+import java.util.EnumMap;
+
+public enum AttendanceInterview {
+	
+	대상자_아님,
+	경고_대상자,
+	면담_대상자,
+	제적_대상자,
+	;
+	
+	public static AttendanceInterview getInterview(EnumMap<AttendanceStatus, Integer> attendanceStatusCount) {
+		int notComeCount = attendanceStatusCount.get(AttendanceStatus.결석) + (attendanceStatusCount.get(AttendanceStatus.지각) / 3);
+		if (notComeCount < 2) {
+			return 대상자_아님;
+		}
+		if (notComeCount < 3) {
+			return 경고_대상자;
+		}
+		if (notComeCount < 5) {
+			return 면담_대상자;
+		}
+		return 제적_대상자;
+	}
+}

ac56ee4fe1f5a1d44d912aa80210ecfef24eebc2
이창근 <dlckdrms0517@gmail.com>
2024-12-14 14:34:15 +0900

    feat: AttendanceEntity를 Crew로 바꾸는 기능 개발

--- src/main/java/attendance/infra/entity/AttendanceEntity.java
+++ src/main/java/attendance/infra/entity/AttendanceEntity.java
@@ -1,23 +1,22 @@
 package attendance.infra.entity;
 
+import attendance.domain.Attendance;
 import attendance.infra.DateTimeConvertor;
 
 import java.time.LocalDateTime;
 
-public class AttendanceEntity implements DatabaseEntity {
-	
-	private final String nickname;
-	private final LocalDateTime attendanceDateTime;
-	
-	public AttendanceEntity(String nickname, LocalDateTime attendanceDateTime) {
-		this.nickname = nickname;
-		this.attendanceDateTime = attendanceDateTime;
-	}
-	
+public record AttendanceEntity(
+		String nickname,
+		LocalDateTime attendanceDateTime
+) implements DatabaseEntity {
 	public static AttendanceEntity from(String line) {
 		String[] lines = line.split(",");
 		String nickname = lines[0];
 		String attendanceDateTime = lines[1];
 		return new AttendanceEntity(nickname, DateTimeConvertor.convertToLocalDateTime(attendanceDateTime));
+	}
+	
+	public Attendance toAttendance() {
+		return Attendance.from(attendanceDateTime);
 	}
 }
--- src/main/java/attendance/infra/repository/domainConvertor/AttendanceEntityToCrewConvertor.java
+++ src/main/java/attendance/infra/repository/domainConvertor/AttendanceEntityToCrewConvertor.java
@@ -1,14 +1,34 @@
 package attendance.infra.repository.domainConvertor;
 
+import attendance.domain.Attendance;
 import attendance.domain.Crew;
 import attendance.infra.entity.AttendanceEntity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AttendanceEntityToCrewConvertor {
 	
 	public static List<Crew> convert(List<AttendanceEntity> attendanceEntities) {
-		//TODO : 구현
-		return null;
+		List<String> nickNames = convertToNickNames(attendanceEntities);
+		List<Crew> crews = new ArrayList<>();
+		for (String nickName : nickNames) {
+			List<Attendance> attendances = convertToAttendances(attendanceEntities, nickName);
+			crews.add(new Crew(nickName, attendances));
+		}
+		return crews;
+	}
+	
+	private static List<String> convertToNickNames(List<AttendanceEntity> attendanceEntities) {
+		return attendanceEntities.stream()
+				.map(AttendanceEntity::nickname)
+				.toList();
+	}
+	
+	private static List<Attendance> convertToAttendances(List<AttendanceEntity> attendanceEntities, String nickName) {
+		return attendanceEntities.stream()
+				.filter(attendanceEntity -> attendanceEntity.nickname().equals(nickName))
+				.map(AttendanceEntity::toAttendance)
+				.toList();
 	}
 }

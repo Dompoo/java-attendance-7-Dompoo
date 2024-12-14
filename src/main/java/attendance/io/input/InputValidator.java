b93174d0951d06b54c8fc4ebc6d0353b5a706f67
이창근 <dlckdrms0517@gmail.com>
2024-12-14 17:13:14 +0900

    fix: 시간 검증 방식 변경

--- src/main/java/attendance/io/input/InputValidator.java
+++ src/main/java/attendance/io/input/InputValidator.java
@@ -1,13 +1,13 @@
 package attendance.io.input;
 
 import attendance.common.CustomExceptions;
+import attendance.common.DateTimeConvertor;
 
 import java.util.regex.Pattern;
 
 public class InputValidator {
 	
 	private static final Pattern NICKNAME_PATTERN = Pattern.compile(".+");
-	private static final Pattern TIME_PATTERN = Pattern.compile("..:..");
 	
 	public void validateNickName(String input) {
 		if (!NICKNAME_PATTERN.matcher(input).matches()) {
@@ -24,7 +24,9 @@ public class InputValidator {
 	}
 	
 	public void validateTime(String input) {
-		if (!TIME_PATTERN.matcher(input).matches()) {
+		try {
+			DateTimeConvertor.convertToLocalTime(input);
+		} catch (Exception e) {
 			throw CustomExceptions.ILLEGAL_FORMAT.get();
 		}
 	}
